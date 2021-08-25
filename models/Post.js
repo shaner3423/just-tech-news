c7d916ebe9c0ae1384e79cd0cfd138bda1906315
Sugar Shane <scrisostomo17@gmail.com>
2021-08-25 00:33:45 -0400

    added

--- models/Post.js
+++ models/Post.js
@@ -37,4 +37,4 @@ Post.init(
 );
 
 
-model.exports = Post;
+module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const Post = require('./Post');
 const User = require('./User');
 
+//create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsToUser(User, {
+    foreignKey: 'user_id',
+});
+
 
 module.exports = { User, Post };
