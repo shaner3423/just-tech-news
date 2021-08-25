b7d8411d26c3a15141b4e8dbef8151b4d4131db0
Sugar Shane <scrisostomo17@gmail.com>
2021-08-25 16:53:13 -0400

    complete and working

--- models/index.js
+++ models/index.js
@@ -1,12 +1,13 @@
 const Post = require('./Post');
 const User = require('./User');
 
+
 //create associations
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
-Post.belongsToUser(User, {
+Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
